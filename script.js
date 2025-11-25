e96d53c68bed79a5105ade56b59c88368ceba5c9
reniejoshi <renie_joshi@yahoo.com>
2025-11-24 22:19:59 -0800

    Add event listener and styles to flip card on click

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// 
 // store pairs in object
+
+const flipCard = document.querySelector('.flip-card');
+flipCard.addEventListener('click', () => {
+    flipCard.classList.add('flip-card-clicked');
+});
